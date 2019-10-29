0c541f5cc1833da59ea14ab893b9da58ff71e887
Aykut Asil <aykuttasil@gmail.com>
2019-10-29 14:40:36 +0300

    refactoring for android platform

--- android/app/src/main/kotlin/com/aykuttasil/modern_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/aykuttasil/modern_flutter/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
 }
