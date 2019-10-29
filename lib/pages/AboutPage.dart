cf932372fb01f50ba5d9031e5e896c90e062b360
Aykut Asil <aykuttasil@gmail.com>
2019-10-29 14:40:49 +0300

    Create AboutPage.dart

---
+++ lib/pages/AboutPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  static const String routeName = '/about';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
